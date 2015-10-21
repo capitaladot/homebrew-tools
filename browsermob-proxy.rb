f5f0f9febc57052fee56738fb2768b0ae2125e33
rubytester <github@rubytester.com>
2015-10-20 21:56:25 -0500

    browsermob-proxy 2.1.0-beta3

--- browsermob-proxy.rb
+++ browsermob-proxy.rb
@@ -1,8 +1,8 @@
 class BrowsermobProxy < Formula
   homepage "http://bmp.lightbody.net/"
-  url "https://s3-us-west-1.amazonaws.com/lightbody-bmp/browsermob-proxy-2.0-beta-9-bin.zip", :using => :curl
-  sha256 "42160d241bbf106c2df6f1199b2d8d9aa63fb79079fd36200cb872e6c942c341"
-  version "2.0-beta-9"
+  url "https://github.com/lightbody/browsermob-proxy/releases/download/browsermob-proxy-2.1.0-beta-3/browsermob-proxy-2.1.0-beta-3-bin.zip", :using => :curl
+  sha256 "4f39694f035db7f9824792af45b5378f798142c0edb8d4790a438be3591d2cbc"
+  version "2.1.0-beta-3"
   def install
     prefix.install Dir["*"]
   end
